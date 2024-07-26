96a32881beffc5bdf39b5f7419d2462d62113312
20023guy <165439341+20023guy@users.noreply.github.com>
2024-07-26 13:09:28 -0400

    Add files via upload

---
+++ mani.js
@@ -0,0 +1,19 @@
+document.getElementById("Normal").addEventListener("click", function() {
+    window.location.href = "normal.html";
+});
+
+document.getElementById("Easy").addEventListener("click", function() {
+    window.location.href = "easy.html";
+});
+document.getElementById("Hard").addEventListener("click", function() {
+    window.location.href = "Hard.html";
+});
+
+document.getElementById("???").addEventListener("click", function() {
+    window.location.href = "What.html";
+});
+
+
+// Alerts for warnings and version
+alert("WARNING: THIS IS BETA VERSION OF THE GAME. BUGS AND GLITCHES EXIST.");
+alert("Welcome to Snake for spc. Version beta 1.2");
